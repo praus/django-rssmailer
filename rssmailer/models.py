c79d4040ae2651b70725c01851a27c2a68c84b24
Petr Praus <petr@praus.net>
2010-11-17 18:41:20 +0100

    Old Item model, no longer necessary.

--- rssmailer/models.py
+++ rssmailer/models.py
@@ -1,20 +1,5 @@
 from django.db import models
 
-#class Item(models.Model):   
-#    title = models.TextField(null=True)
-#    description = models.TextField(null=True)
-#    guid = models.CharField(max_length=255, null=True)
-#    updated = models.DateTimeField(null=True)
-#    
-#    def __init__(self, title=None, desc=None, guid=None, updated=None):
-#        self.title = title
-#        self.description = desc
-#        self.guid = guid
-#        self.updated = updated
-#    
-#    def __unicode__(self):
-#        return self.title
-
 class Channel(models.Model):
     '''Model for channels to crawl.'''
     url = models.URLField(max_length=255)
