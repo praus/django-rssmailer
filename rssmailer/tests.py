f4e7a68017f55c008a1ad8bed04210ad3d77f6a9
Petr Praus <petr@praus.net>
2010-11-19 13:35:37 +0100

    matcher() unit test

--- rssmailer/tests.py
+++ rssmailer/tests.py
@@ -1,23 +1,36 @@
-"""
-This file demonstrates two different styles of tests (one doctest and one
-unittest). These will both pass when you run "manage.py test".
-
-Replace these with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
+from models import EntryHash
+from tasks import feeder
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
-"""}
+class MatcherTest(TestCase):
+    def setUp(self):
+        self.entries = [
+            {'title': '1'},
+            {'title': '2'},
+            {'title': '3'},
+            {'title': '4'},
+            {'title': '5'},
+        ]
+        
+        self.seen_hashes = [
+        'c4ca4238a0b923820dcc509a6f75849b', # 1
+        #'c81e728d9d4c2f636f067f89cc14862c', # 2
+        'eccbc87e4b5ce2fe28308fd9f2a7baf3', # 3
+        #'a87ff679a2f3e71d9181a67b7542122c', # 4
+        'e4da3b7fbbce2345d7772b0674a318d5', # 5
+        ]
+        self.seen = map(lambda hash: EntryHash(hash).save(), self.seen_hashes)
+        
+    def test_matcher(self):
+        """
+        Tests that matcher correctly identifies new and seen entries.
+        """
+        new_entries = feeder.matcher(self.entries)
+        new_hashes = map(lambda e: e[0], new_entries)
+        
+        self.failUnless(len(new_entries) == 2 and
+                    new_hashes[0] == 'c81e728d9d4c2f636f067f89cc14862c' and
+                    new_hashes[1] == 'a87ff679a2f3e71d9181a67b7542122c',
+                    "new_entries has wrong number of elements or wrong elements.")
 
